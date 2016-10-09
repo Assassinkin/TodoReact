ee6c637b9789c2c770fedebde4049cb711b6c12a
Assassinkin <iuns@outlook.fr>
2016-10-09 20:10:46 +0100

    Move fire base credentilas to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 try {
   var config = {
-     apiKey: "AIzaSyBZ1RI50FjbfOQGo7c4LNA5zo5IJRQTwoU",
-     authDomain: "psy-todo-app.firebaseapp.com",
-     databaseURL: "https://psy-todo-app.firebaseio.com",
-     storageBucket: "psy-todo-app.appspot.com",
-     messagingSenderId: "254990026562"
+     apiKey: process.env.API_KEY,
+     authDomain: process.env.AUTH_DOMAIN,
+     databaseURL: process.env.DATABASE_URL,
+     storageBucket: process.env.STORAGE_BUCKET,
+     messagingSenderId: process.env.MESSAGING_SENDER_ID
    };
   firebase.initializeApp(config);
 } catch(e) {
